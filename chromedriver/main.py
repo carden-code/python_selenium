f150b456e3d658863914cda2e5913b9c9ea6c9a8
Carden <carden.ruby@gmail.com>
2021-04-13 15:57:01 +0300

    refrash(), save_screenshot()

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -3,13 +3,24 @@ from time import sleep
 
 
 url = 'https://instagram.com/'
-driver = webdriver.Chrome(executable_path='absolute path chromedriver')
+driver = webdriver.Chrome(executable_path='/home/vyacheslav/python_selenium/chromedriver/chromedriver')
 
 try:
     driver.get(url=url)
     sleep(5)
-    # driver.get('https://stackoverflow.com/')
-    # sleep(5)
+
+    # refrash - обновляет окно браузера.
+    # driver.refresh()
+
+    # Делает скриншот страницы и сохраняет её.
+    driver.get_screenshot_as_file('1.png')
+
+    driver.get('https://stackoverflow.com/')
+    sleep(5)
+
+    # Делает скриншот страницы и сохраняет её.
+    driver.save_screenshot('2.png')
+    sleep(2)
 except Exception as ex:
     print(ex)
 finally:
