356de757742045273574e3fa63b49238d7d3495c
Carden <carden.ruby@gmail.com>
2021-04-23 21:16:27 +0300

    add cookies in webdriver chrome

---
+++ lesson_5_cookies/cookies_chrome.py
@@ -0,0 +1,58 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# from fake_useragent import UserAgent
+from auth_data import *
+import pickle
+
+#
+# user_agent = UserAgent()
+
+# Создаём объект Опций
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+
+driver = webdriver.Chrome(executable_path='/home/vyacheslav/python_selenium/lesson_1/chromedriver/chromedriver',
+                          options=options)
+
+try:
+    # driver.get('https://vk.com')
+    # sleep(5)
+    #
+    # email_input = driver.find_element_by_id("index_email")
+    # # Очищает поле
+    # email_input.clear()
+    # email_input.send_keys(vk_name)
+    # sleep(5)
+    #
+    # password_input = driver.find_element_by_xpath('//*[@id="index_pass"]')
+    # password_input.clear()
+    # password_input.send_keys(vk_password)
+    # sleep(3)
+    # password_input.send_keys(Keys.ENTER)
+    # # login_button = driver.find_element_by_id('index_login_button')
+    # # login_button.click()
+    # sleep(20)
+    #
+    # news_link = driver.find_element_by_id('l_nwsf').click()
+    # sleep(5)
+    #
+    # # сохраняем cookies в файл
+    # pickle.dump(driver.get_cookies(), open(f"{vk_name}_cookies", "wb"))
+    driver.get('https://vk.com')
+    sleep(5)
+
+    # Открываем файл с кук и добавляем в вебдрайвер
+    for cookie in pickle.load(open(f"{vk_name}_cookies", "rb")):
+        driver.add_cookie(cookie)
+    sleep(5)
+
+    driver.refresh()
+    sleep(10)
+except Exception as ex:
+    print(ex)
+finally:
+    # Закрывает окно браузера
+    driver.close()
+    driver.quit()
